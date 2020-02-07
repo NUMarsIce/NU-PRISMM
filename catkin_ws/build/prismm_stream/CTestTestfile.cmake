bacd01e21f9eb438d4137fc4e475f0e2017ba2e4
Ian <spyguyian@gmail.com>
2020-02-06 21:13:19 -0800

    launch for 2 hd cameras is up

---
+++ catkin_ws/build/prismm_stream/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/nu-prismm/catkin_ws/src/prismm_stream
+# Build directory: /home/pi/nu-prismm/catkin_ws/build/prismm_stream
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
